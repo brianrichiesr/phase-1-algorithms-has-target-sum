1b5b4d49fb35b72ebf059f0851684aead11a923b
Brian Richie Sr <brianrichiesr@gmail.com>
2023-10-01 09:54:45 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  /* (n steps: depending on the size of the array) */
+  for (let i = 0; i < array.length; i++) {
+    /* (n steps: depending on the size of the array) */
+    const solutionNumber = target - array[i];
+    /* (n steps: depending on the size of the array) * 2 conditionals */
+    if (array.includes(solutionNumber) && array.indexOf(solutionNumber) !== i) {
+      return true;
+    }
+  }
+  /* 1 step */
+  return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  * n steps for the for loop because I don't know the size of the array
+  * n steps for the assignment of a value for 'solutionNumber' because this one step happens each iteration and I don't know the size of the array
+  * 2n steps, one n for each condition checked, because the conditions are checked on each iteration and I don't know the size of the array
+  * 1 step that can potentially happen if the condition in the for loop does not return true
+  * 
+  * Big O = O(4n + 1)
+  * 
+  * Remove Constants = O(n)
 */
 
 /* 
-  Add your pseudocode here
+  Iterate through the array
+    Figure out what number is needed to add to the index of the iteration to equal 'target'
+    Figure out if number needed is included in the array
+    Make sure the number needed is not the same index
+    Return true if the number needed is in the array
+  Return false if it isn't
 */
 
 /*
-  Add written explanation of your solution here
+  * I have to iterate through 'array' to perform an evaluation on each number in 'array' and I prefer for loops
+  * At each iteration, I assign to a variable the result of subtracting the current index from 'target' in order to determine what number is needed to add to the current index to equal 'target'
+  * I use the '.includes()' method to determine if the number needed is included in 'array'
+  * If the needed number is included in 'array', return true
+  * If the for loop complete and no solution is found, return false
 */
 
 // You can run `node index.js` to view these console logs
